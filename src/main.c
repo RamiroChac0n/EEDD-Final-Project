0c8c964dcbe0e623ca0710e2a518efafbb689537
Ramiro Chacon <ramiro20010202@gmail.com>
2023-05-16 20:38:28 +0000

    Completa buscarDocente()

--- src/main.c
+++ src/main.c
@@ -17,13 +17,13 @@ typedef struct{
     Fecha anioObtencion;
 }Titulo;
 
-//Crea un nodo de arbol binario de titulos universitarios.
+//Crea un nodo de un arbol binario de titulos universitarios.
 typedef struct nodo_titulos{
     Titulo titulo;
     int altura;
-    struct arbol_titulos *izquierda;
-    struct arbol_titulos *derecha;
-}Arbol_titulos;
+    struct nodo_titulos *izquierda;
+    struct nodo_titulos *derecha;
+}Nodo_titulos;
 
 //Crea una estructura que representa a un docente con los siguientes campos: id, nombre, apellido, edad, telefono, ciudad, arbol de titulos universitarios.
 typedef struct{
@@ -33,7 +33,7 @@ typedef struct{
     int edad;
     char telefono[50];
     char ciudad[50];
-    Arbol_titulos *arbolTitulos;
+    Nodo_titulos *arbolTitulos;
 }Docente;
 
 //Crea una estructura que representa un nodo de una lista enlazada de docentes.
@@ -50,26 +50,139 @@ typedef struct{
     Nodo_docente *listaDocentes;
 }Universidad;
 
-//Crea un método que pide los datos de un titulo universitario y lo retorna.
-Titulo crearTitulo(){
-    Titulo titulo;
-    printf("Ingrese el id del titulo:\n");
-    scanf("%d", &titulo.id);
-    fflush(stdin);
-    printf("Ingrese el nombre del titulo:\n");
-    fgets(titulo.nombre, 50, stdin);
-    printf("Ingrese el nombre de la universidad:\n");
-    fgets(titulo.universidad, 50, stdin);
-    printf("Ingrese el dia de obtencion del titulo:\n");
-    scanf("%d", &titulo.anioObtencion.dia);
-    fflush(stdin);
-    printf("Ingrese el mes de obtencion del titulo:\n");
-    scanf("%d", &titulo.anioObtencion.mes);
-    fflush(stdin);
-    printf("Ingrese el anio de obtencion del titulo:\n");
-    scanf("%d", &titulo.anioObtencion.anio);
-    fflush(stdin);
-    return titulo;
+/*********** Arbol de titulos *************/
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int altura(Nodo_titulos *nodo) {
+    if (nodo == NULL)
+        return 0;
+    return nodo->altura;
+}
+
+Nodo_titulos* nuevoNodo(Nodo_titulos *nodo_padre, int id) {
+    Nodo_titulos* nodo = (Nodo_titulos*)malloc(sizeof(Nodo_titulos));
+    nodo->titulo.id = id;
+    nodo->izquierda = NULL;
+    nodo->derecha = NULL;
+    nodo->altura = 1;
+    return nodo;
+}
+
+Nodo_titulos *rotacionDerecha(Nodo_titulos *y) {
+    Nodo_titulos *x = y->izquierda;
+    Nodo_titulos *T2 = x->derecha;
+
+    x->derecha = y;
+    y->izquierda = T2;
+
+    y->altura = max(altura(y->izquierda), altura(y->derecha)) + 1;
+    x->altura = max(altura(x->izquierda), altura(x->derecha)) + 1;
+
+    return x;
+}
+
+Nodo_titulos *rotacionIzquierda(Nodo_titulos *x) {
+    Nodo_titulos *y = x->derecha;
+    Nodo_titulos *T2 = y->izquierda;
+
+    y->izquierda = x;
+    x->derecha = T2;
+
+    x->altura = max(altura(x->izquierda), altura(x->derecha)) + 1;
+    y->altura = max(altura(y->izquierda), altura(y->derecha)) + 1;
+
+    return y;
+}
+
+int balance(Nodo_titulos *nodo) {
+    if (nodo == NULL)
+        return 0;
+    return altura(nodo->izquierda) - altura(nodo->derecha);
+}
+
+Nodo_titulos* insertar_titulo(Nodo_titulos* nodo, int clave) {
+    if (nodo == NULL)
+        return (nuevoNodo(nodo, clave)); //Crea un nuevo nodo si el arbol esta vacio.
+
+    if (clave < nodo->titulo.id)
+        nodo->izquierda = insertar_titulo(nodo->izquierda, clave);
+    else if (clave > nodo->titulo.id)
+        nodo->derecha = insertar_titulo(nodo->derecha, clave);
+    else
+        return nodo;
+
+    nodo->altura = 1 + max(altura(nodo->izquierda), altura(nodo->derecha));
+
+    int balanceo = balance(nodo);
+
+    if (balanceo > 1 && clave < nodo->izquierda->titulo.id)
+        return rotacionDerecha(nodo);
+
+    if (balanceo < -1 && clave > nodo->derecha->titulo.id)
+        return rotacionIzquierda(nodo);
+
+    if (balanceo > 1 && clave > nodo->izquierda->titulo.id) {
+        nodo->izquierda = rotacionIzquierda(nodo->izquierda);
+        return rotacionDerecha(nodo);
+    }
+
+    if (balanceo < -1 && clave < nodo->derecha->titulo.id) {
+        nodo->derecha = rotacionDerecha(nodo->derecha);
+        return rotacionIzquierda(nodo);
+    }
+
+    return nodo;
+}
+/******************************************/
+
+//Crea un método para crear un titulo universitario y agregarlo al arbol de titulos de un docente de una universidad.
+void agregarTitulo(Universidad *Universidad){
+    int idDocente, idTitulo;
+    Nodo_docente *nodoDocente = Universidad->listaDocentes;
+    Nodo_titulos *nodoTitulos;
+    printf("Ingrese el id del docente: ");
+    scanf("%d", &idDocente);
+    while(nodoDocente != NULL){
+        if(nodoDocente->docente.id == idDocente){
+            printf("Ingrese el id del titulo: ");
+            scanf("%d", &idTitulo);
+            nodoTitulos = insertar_titulo(nodoDocente->docente.arbolTitulos, idTitulo);
+            nodoDocente->docente.arbolTitulos = nodoTitulos;
+            printf("Titulo agregado correctamente.\n");
+            return;
+        }
+        nodoDocente = nodoDocente->siguiente;
+    }
+    printf("No se encontro el docente.\n");
+}
+
+//Método para mostrar la información de un titulo.
+void mostrarTitulo(Titulo titulo){
+    printf("%-5d", titulo.id);
+    printf("%-50s", titulo.nombre);
+    printf("%-50s", titulo.universidad);
+    printf("%02d/%02d/%04d\n", titulo.anioObtencion.dia, titulo.anioObtencion.mes, titulo.anioObtencion.anio);
+}
+
+
+//Crea un método para mostrar los id de los titulos en forma de arbol binario.
+void mostrarArbolTitulosID(Nodo_titulos *nodo){
+    if(nodo != NULL){
+        mostrarArbolTitulosID(nodo->izquierda);
+        printf("%d ", nodo->titulo.id);
+        mostrarArbolTitulosID(nodo->derecha);
+    }
+}
+
+//Muestra los titulos de un docente.
+void mostrarTitulos(Nodo_titulos *nodo) {
+    if (nodo != NULL) {
+        mostrarTitulos(nodo->izquierda);
+        mostrarTitulo(nodo->titulo);
+        mostrarTitulos(nodo->derecha);
+    }
 }
 
 //Imprime la informacion de un docente.
@@ -82,6 +195,8 @@ void imprimirDocente(Docente docente){
     printf("Telefono: %s ", docente.telefono);
     printf("Ciudad: %s\n", docente.ciudad);
     printf("\n--------------- Titulos ---------------------\n");
+    //Recorre el arbol de titulos y muestra la informacion de cada titulo.
+    mostrarTitulos(docente.arbolTitulos);
     printf("---------------------------------------------\n");
 }
 
@@ -227,6 +342,34 @@ void docentesQuemados(Universidad *universidad){
     docente1->docente.edad = 30;
     strcpy(docente1->docente.telefono, "123456");
     strcpy(docente1->docente.ciudad, "Cochabamba");
+    //Agrega un titulo al docente 1
+    Nodo_titulos *titulo1 = (Nodo_titulos*)malloc(sizeof(Nodo_titulos));
+    titulo1->titulo.id = 10;
+    strcpy(titulo1->titulo.nombre, "Licenciatura en Sistemas");
+    strcpy(titulo1->titulo.universidad, "UMSS");
+    titulo1->titulo.anioObtencion.dia = 1;
+    titulo1->titulo.anioObtencion.mes = 1;
+    titulo1->titulo.anioObtencion.anio = 2010;
+    //Agrega otro titulo al docente 1
+    Nodo_titulos *titulo2 = (Nodo_titulos*)malloc(sizeof(Nodo_titulos));
+    titulo2->titulo.id = 20;
+    strcpy(titulo2->titulo.nombre, "Ingenieria en Sistemas");
+    strcpy(titulo2->titulo.universidad, "San Simon");
+    titulo2->titulo.anioObtencion.dia = 1;
+    titulo2->titulo.anioObtencion.mes = 1;
+    titulo2->titulo.anioObtencion.anio = 2000;
+    //Agrega otro titulo al docente 1
+    Nodo_titulos *titulo3 = (Nodo_titulos*)malloc(sizeof(Nodo_titulos));
+    titulo3->titulo.id = 30;
+    strcpy(titulo3->titulo.nombre, "Maestria en Sistemas");
+    strcpy(titulo3->titulo.universidad, "Universitat de Barcelona");
+    titulo3->titulo.anioObtencion.dia = 1;
+    titulo3->titulo.anioObtencion.mes = 1;
+    titulo3->titulo.anioObtencion.anio = 2015;
+    titulo2->izquierda = titulo1;
+    titulo2->derecha = titulo3;
+    docente1->docente.arbolTitulos = titulo2;
+
     //Docente 2
     Nodo_docente *docente2 = (Nodo_docente*)malloc(sizeof(Nodo_docente));
     docente2->docente.id = 10;
@@ -235,6 +378,34 @@ void docentesQuemados(Universidad *universidad){
     docente2->docente.edad = 25;
     strcpy(docente2->docente.telefono, "123456");
     strcpy(docente2->docente.ciudad, "Cochabamba");
+    //Agregar un titulo al docente 2
+    Nodo_titulos *titulo4 = (Nodo_titulos*)malloc(sizeof(Nodo_titulos));
+    titulo4->titulo.id = 10;
+    strcpy(titulo4->titulo.nombre, "Licenciatura en Sistemas");
+    strcpy(titulo4->titulo.universidad, "UMSS");
+    titulo4->titulo.anioObtencion.dia = 1;
+    titulo4->titulo.anioObtencion.mes = 1;
+    titulo4->titulo.anioObtencion.anio = 2010;
+    //Agregar otro titulo al docente 2
+    Nodo_titulos *titulo5 = (Nodo_titulos*)malloc(sizeof(Nodo_titulos));
+    titulo5->titulo.id = 20;
+    strcpy(titulo5->titulo.nombre, "Ingenieria en Sistemas");
+    strcpy(titulo5->titulo.universidad, "San Simon");
+    titulo5->titulo.anioObtencion.dia = 1;
+    titulo5->titulo.anioObtencion.mes = 1;
+    titulo5->titulo.anioObtencion.anio = 2000;
+    //Agregar otro titulo al docente 2
+    Nodo_titulos *titulo6 = (Nodo_titulos*)malloc(sizeof(Nodo_titulos));
+    titulo6->titulo.id = 30;
+    strcpy(titulo6->titulo.nombre, "Maestria en Sistemas");
+    strcpy(titulo6->titulo.universidad, "Universitat de Barcelona");
+    titulo6->titulo.anioObtencion.dia = 1;
+    titulo6->titulo.anioObtencion.mes = 1;
+    titulo6->titulo.anioObtencion.anio = 2015;
+    titulo5->izquierda = titulo4;
+    titulo5->derecha = titulo6;
+    docente2->docente.arbolTitulos = titulo5;
+
     //Docente 3
     Nodo_docente *docente3 = (Nodo_docente*)malloc(sizeof(Nodo_docente));
     docente3->docente.id = 15;
@@ -247,6 +418,42 @@ void docentesQuemados(Universidad *universidad){
     docente2->siguiente = docente3;
     docente3->siguiente = NULL;
     universidad->listaDocentes = docente1;
+    //Agrega un titulo al docente 3
+    Nodo_titulos *titulo7 = (Nodo_titulos*)malloc(sizeof(Nodo_titulos));
+    titulo7->titulo.id = 10;
+    strcpy(titulo7->titulo.nombre, "Licenciatura en Sistemas");
+    strcpy(titulo7->titulo.universidad, "UMSS");
+    titulo7->titulo.anioObtencion.dia = 1;
+    titulo7->titulo.anioObtencion.mes = 1;
+    titulo7->titulo.anioObtencion.anio = 2010;
+    //Agregar otro titulo al docente 3
+    Nodo_titulos *titulo8 = (Nodo_titulos*)malloc(sizeof(Nodo_titulos));
+    titulo8->titulo.id = 20;
+    strcpy(titulo8->titulo.nombre, "Ingenieria en Sistemas");
+    strcpy(titulo8->titulo.universidad, "San Simon");
+    titulo8->titulo.anioObtencion.dia = 1;
+    titulo8->titulo.anioObtencion.mes = 1;
+    titulo8->titulo.anioObtencion.anio = 2000;
+    //Agregar otro titulo al docente 3
+    Nodo_titulos *titulo9 = (Nodo_titulos*)malloc(sizeof(Nodo_titulos));
+    titulo9->titulo.id = 30;
+    strcpy(titulo9->titulo.nombre, "Maestria en Sistemas");
+    strcpy(titulo9->titulo.universidad, "Universitat de Barcelona");
+    titulo9->titulo.anioObtencion.dia = 1;
+    titulo9->titulo.anioObtencion.mes = 1;
+    titulo9->titulo.anioObtencion.anio = 2015;
+    //Agrega otro titulo al docente 3
+    Nodo_titulos *titulo10 = (Nodo_titulos*)malloc(sizeof(Nodo_titulos));
+    titulo10->titulo.id = 25;
+    strcpy(titulo10->titulo.nombre, "Doctorado en Sistemas");
+    strcpy(titulo10->titulo.universidad, "Universitat de Barcelona");
+    titulo10->titulo.anioObtencion.dia = 1;
+    titulo10->titulo.anioObtencion.mes = 1;
+    titulo10->titulo.anioObtencion.anio = 2017;
+    titulo9->izquierda = titulo10;
+    titulo8->izquierda = titulo7;
+    titulo8->derecha = titulo9;
+    docente3->docente.arbolTitulos = titulo8;
 }
 /* ******* TEST ******* */
 
@@ -263,7 +470,8 @@ int main(){
         printf("1. Agregar docente.\n");
         printf("2. Buscar docente\n");
         printf("3. Eliminar docente.\n");
-        printf("5. SALIR.\n");
+        printf("4. Agregar título.\n");
+        printf("10. SALIR.\n");
         printf("Ingrese una opcion:\n");
         scanf("%d", &opcion);
         switch(opcion){
@@ -279,7 +487,10 @@ int main(){
             case 3:
                 eliminarDocente(universidad);
                 break;
-            case 5:
+            case 4:
+                agregarTitulo(universidad);
+                break;
+            case 10:
                 printf("Adios.\n");
                 break;
             default:
